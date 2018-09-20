06cf7d02bb548ad165366e8f557cd01a7a07a6b7
muguira-james <muguira.james@gmail.com>
2018-09-20 17:46:11 -0400

    pretty up the comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ This project was started with create-react-app
 
 This is the base project.  Only enough to show the basic idea.
 
-ShowMap is a component that displays a leaflet based map.  the project is 
+ShowMap is a component that displays a leaflet based map.  The project is 
 designed to show how to combine React and a map that does not have license issues.
 
 The course file is a simple json object that describes where the attributes of the course
